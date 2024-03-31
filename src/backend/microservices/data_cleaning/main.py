c0fb9eaa19c345d6639edfdb40c679f3b54f180e
Jarko <jarkoarenal@gmail.com>
2024-04-01 00:52:45 +0200

    Modificado el flujo de trabajo de la rama data-cleaning, los cambios se han realizado en los archivos: - src/backend/microservices/data_cleaning/data_cleaning_functions.py - src/backend/microservices/data_cleaning/main.py - src/backend/microservices/data_cleaning/readme.txt - src/backend/microservices/data_cleaning/requirements.txt
    Se han hecho que las funciones de limpieza de datos devuelvan la ruta al nuevo archivo modificado

--- src/backend/microservices/data_cleaning/main.py
+++ src/backend/microservices/data_cleaning/main.py
@@ -5,6 +5,15 @@ import load_dataset as ld
 import data_cleaning_functions as dcf
 from typing import Optional, List, Dict
 import numpy as np
+import logging
+import pandas as pd
+
+# Inicializa el logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Estructura para mantener el estado de los datasets en memoria
+datasets_state = {}
 
 class CleaningRequest(BaseModel):
     file_name: str
@@ -22,12 +31,16 @@ class CleaningRequest(BaseModel):
     string_operations: Optional[str] = None
     regex_pattern: Optional[str] = None
     regex_replacement: Optional[str] = None
+    deshacer: Optional[bool] = False
 
 app = FastAPI()
 
+
 @app.post("/apply_cleaning")
 async def apply_cleaning_operation(request: CleaningRequest):
-    options = request.dict(exclude={'file_name'})
+    global ultimo_estado # Accede al estado global
+
+    options = request.dict(exclude={'file_name', 'deshacer'})
 
     # Definir la ruta de almacenamiento y comprobar si el archivo existe
     storage_path = "/files"
@@ -35,13 +48,30 @@ async def apply_cleaning_operation(request: CleaningRequest):
 
     if not os.path.exists(file_location):
         raise HTTPException(status_code=404, detail="Archivo no encontrado")
-
+    
     try:
         df = await ld.load_data(file_location)
         result = dcf.data_cleaning(df, options)
-        if isinstance(result, np.int64):
-            result = int(result)  # Convierte np.int64 a int nativo de Python
+        logger.info(f"Funcion Datacleaning completa, enviando resultado fuera de la API, Dataset ID: {request.file_name}")
+
+        # Maneja diferentes tipos de resultados
+        if isinstance(result, pd.DataFrame):
+            # Si el resultado es un DataFrame, se convierte a un formato específico (por ejemplo, JSON)
+            result = result.to_json(orient="records")
+            logger.info("DataFrame convertido a JSON")
+
+        elif isinstance(result, np.int64) or isinstance(result, np.float64):
+            # Convierte np.int64 o np.float64 a tipos nativos de Python
+
+            result = result.item()  # Otra opción es result = int(result) si sabes que es siempre entero
+            logger.info("Número Numpy convertido a tipo nativo")
+            # Puedes agregar más condiciones según sea necesario para otros tipos de datos
+
         return {"message": "Operaciones aplicadas con éxito", "result": result}
+
+   
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+ 
--- src/backend/microservices/dataset_loading/main.py
+++ src/backend/microservices/dataset_loading/main.py
@@ -2,32 +2,25 @@ from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Depends, Sec
 from fastapi.security.api_key import APIKeyHeader
 import shutil
 import os
+import redis
+from io import BytesIO
+import pandas as pd
+import load_dataset as ld
+import pyarrow as pa
 
 # Lanza la api 
 app = FastAPI()
 
-# Configuración simple de seguridad basada en token, solo para desarrollo
-
-API_KEY = "tokencitotokencito"
-API_KEY_NAME = "api_token"
-api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
-
-# Función para verificar el token de acceso
-async def get_api_key(api_key_header: str = Security(api_key_header)):
-    if api_key_header == API_KEY:
-        return True
-    else:
-        raise HTTPException(status_code=403, detail="Token de acceso inválido")
-    
-
+# Conectar a Redis
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 #Endpoint para subir un archivo y almacenarlo en el servidor
 
-@app.post("/upload-dataset/")
-async def upload_dataset(file: UploadFile = File(...)):
+@app.post("/upload-dataset/{session_id}")
+async def upload_dataset(session_id: str, file: UploadFile = File(...)):
    
     # Definir la ruta de almacenamiento
-    storage_path = "/files"
+    storage_path = f"/files/{session_id}"
     file_location = f"{storage_path}/{file.filename}"
     
     # Verificar que el archivo sea un CSV o un Excel o un JSON
@@ -40,7 +33,15 @@ async def upload_dataset(file: UploadFile = File(...)):
             # Guardar archivo en el almacenamiento
             with open(file_location, "wb+") as file_object:
                 shutil.copyfileobj(file.file, file_object)
+            
+            # Carga de un DataFrame desde un archivo
+            df = ld(file)
 
+            # Serializar el DataFrame para almacenamiento en Redis
+            context = pa.default_serialization_context()
+
+            r.set("key", context.serialize(df).to_buffer().to_pybytes())
+            context.deserialize(r.get("key"))
 
            # Lugar para la lógica de notificación al servicio de destino
             
@@ -52,8 +53,6 @@ async def upload_dataset(file: UploadFile = File(...)):
             raise HTTPException(status_code=500, detail=str(e))
     else:
         raise HTTPException(status_code=400, detail="Formato de archivo no soportado.")
-
-
 
 
 
