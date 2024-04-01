4111ee0ae34d7e94c34de50b13016e67fc7dadc5
Jarko <jarkoarenal@gmail.com>
2024-04-01 02:47:44 +0200

    Se han añadido los ficheros aux_functions.py y config.py a la carpeta data_cleaning y se han modificado los ficheros main.py de data_cleaning y dataset_loading. Además, se han añadido los ficheros aux_functions.py y config.py a la carpeta dataset_loading y se ha modificado el fichero requirements.txt de dataset_loading.
    aux_functions: Contiene las funciones auxiliares que se utilizan en los microservicios de data_cleaning y data_loading.
    config.py: Contiene la carga de variables de configuración que se utilizan en los microservicios de data_cleaning y data_loading.
    Se ha añadido gitignore a la carpeta dataset_loading para que no se suba el fichero env
    
    En esta version se ha añadido el sistema de listar datasets del usuario, de sobrescribir datasets y de cargar datasets.
    Se han añadido funciones para actualizar la base de datos segun corresponda.

--- src/backend/microservices/dataset_loading/main.py
+++ src/backend/microservices/dataset_loading/main.py
@@ -7,57 +7,92 @@ from io import BytesIO
 import pandas as pd
 import load_dataset as ld
 import pyarrow as pa
+import logging
+from typing import List
+from pydantic import BaseModel
+import aux_functions as af
+
+# Inicializa el logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Lanza la api 
 app = FastAPI()
 
-# Conectar a Redis
-r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+# clase para el modelo de datos
+class Dataset(BaseModel):
+    data_id: str
+    file_name: str
 
-#Endpoint para subir un archivo y almacenarlo en el servidor
+#Endpoint para subir un archivo y almacenarlo en el servidor (Falta implementar el LOGGING)
 
-@app.post("/upload-dataset/{session_id}")
-async def upload_dataset(session_id: str, file: UploadFile = File(...)):
-   
-    # Definir la ruta de almacenamiento
-    storage_path = f"/files/{session_id}"
+@app.post("/upload-dataset/{user_id}")
+
+# Se espera recibir un user_id, un archivo y un booleano para sobreescribir o no (Por defecto no sobreescribe)
+# Pendiente de añadir que se envie tambien un nombre para el archivo, en vez de usar el que tiene el archivo por defecto
+async def upload_dataset(user_id: str, file: UploadFile = File(...), overwrite: bool = False):
+
+    # Ruta donde se almacenarán los archivos
+    storage_path = f"/files/{user_id}"
+
+    # Crear la carpeta si no existe
+    if not os.path.exists(storage_path):
+        os.makedirs(storage_path)
+
+    # Ruta al archivo final
     file_location = f"{storage_path}/{file.filename}"
-    
-    # Verificar que el archivo sea un CSV o un Excel o un JSON
-    if file.content_type in ["text/csv",
-                            "application/vnd.ms-excel",
-                            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                            "application/json"]:
+
+    # Verificar si el archivo ya existe
+    existing_file = af.check_file_exists(user_id, file.filename)
+
+    # Si el archivo ya existe y no se ha confirmado la sobrescritura, devuelve un mensaje para confirmar
+    if existing_file and not overwrite:
+        return {"message": "El archivo ya existe. ¿Desea sobreescribir?", "overwrite_required": True}
+
+    # Si el archivo es un CSV, Excel o JSON, lo guardamos en el servidor
+    if file.content_type in ["text/csv", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/json"]:
         try:
-        
-            # Guardar archivo en el almacenamiento
+            # Guardar el archivo en el servidor
             with open(file_location, "wb+") as file_object:
                 shutil.copyfileobj(file.file, file_object)
-            
-            # Carga de un DataFrame desde un archivo
-            df = ld(file)
 
-            # Serializar el DataFrame para almacenamiento en Redis
-            context = pa.default_serialization_context()
+            # Si estamos sobreescribiendo, actualizamos la entrada existente en lugar de crear una nueva
+            if existing_file:
+                af.update_file_mapping(user_id, file.filename, file_location) # Falta implementar esta función
 
-            r.set("key", context.serialize(df).to_buffer().to_pybytes())
-            context.deserialize(r.get("key"))
+            # Si no estamos sobreescribiendo, creamos una nueva entrada
+            else:
+                data_id = af.generate_data_id()
+                af.insert_file_mapping(data_id, file_location)
 
-           # Lugar para la lógica de notificación al servicio de destino
-            
-            # Por ahora, solo devolvemos la ruta del archivo
-            return {"location": file_location}
-        
+            message = "Archivo subido correctamente"
+
+            # Devolver el ID del archivo y un mensaje de confirmación
+            return {"data_id": data_id, "message": message}
+
         except Exception as e:
-            print(e)
-            raise HTTPException(status_code=500, detail=str(e))
+            raise HTTPException(status_code=500, detail=f"Error al guardar el archivo: {e}")
     else:
         raise HTTPException(status_code=400, detail="Formato de archivo no soportado.")
 
 
 
+@app.get("/list_datasets/{user_id}", response_model=List[Dataset])
+async def list_datasets_for_user(user_id: str):
 
-        
+    # Obtener la lista de datasets del usuario
+    datasets = af.get_files_list(user_id)
+
+    # Si no hay datasets, devolver un error 404
+    if not datasets:
+        raise HTTPException(status_code=404, detail="No se han encontrado datasets almacenados")
+    
+    # Si hay datasets, devolver una lista con los IDs y nombres de los archivos
+    return [{"data_id": data_id, "file_name": file_path.split("/")[-1]} for data_id, file_path in datasets]
+
+
+
+
 
 
 
