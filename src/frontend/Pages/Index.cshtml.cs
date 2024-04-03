bb21c00826ca10a57bdcf21cedc69f2baa0c5928
Jarko <jarkoarenal@gmail.com>
2024-04-03 22:37:36 +0200

    Actualizando archivos testing

--- src/frontend/Pages/Index.cshtml.cs
+++ src/frontend/Pages/Index.cshtml.cs
@@ -22,31 +22,37 @@ public class IndexModel : PageModel
 
     public async Task<IActionResult> OnPostAsync()
     {
-        try{
-        if (UploadedFile != null && UploadedFile.Length > 0)
+        try
         {
-            var httpClient = _httpClientFactory.CreateClient();
-            var form = new MultipartFormDataContent();
-            var streamContent = new StreamContent(UploadedFile.OpenReadStream());
-            form.Add(streamContent, "file", UploadedFile.FileName);
-
-            var userId = "1"; // AQUI EL ID DE USUARIO
-            var response = await httpClient.PostAsync($"http://127.0.0.1:8000/upload-dataset/{userId}", form);
-
-            if (response.IsSuccessStatusCode)
+            if (UploadedFile != null && UploadedFile.Length > 0)
             {
-                TempData["Message"] = "Archivo subido con éxito.";
-                return RedirectToPage("/Index");
+                var httpClient = _httpClientFactory.CreateClient();
+                var form = new MultipartFormDataContent();
+                var streamContent = new StreamContent(UploadedFile.OpenReadStream());
+                form.Add(streamContent, "file", UploadedFile.FileName);
+                form.Add(new StringContent("False"), "overwrite");
+
+                var userId = "1"; // AQUI EL ID DE USUARIO
+                var response = await httpClient.PostAsync($"http://data_loading:8000/upload-dataset/{userId}", form);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["Message"] = "Archivo subido con éxito.";
+                    return RedirectToPage("/Index");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "No se ha podido subir el archivo.";
+                    return RedirectToPage("/Index");
+                }
             }
             else
             {
-                TempData["ErrorMessage"] = "No se ha podido subir el archivo.";
-                return RedirectToPage("/Index");
+                TempData["ErrorMessage"] = "No se ha seleccionado ningún archivo.";
             }
-        }else{
-            TempData["ErrorMessage"] = "No se ha seleccionado ningún archivo.";
         }
-        }catch(Exception e){
+        catch (Exception e)
+        {
             TempData["ErrorMessage"] = "Ha ocurrido un problema.";
             return RedirectToPage("/Index");
         }
